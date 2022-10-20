88201b916a98258a3a619c35a527d85c28542f86
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-10-20 15:15:27 +0900

    WIP

--- matlab_ws/simulation.m
+++ matlab_ws/simulation.m
@@ -38,7 +38,7 @@ eqn=functionalDerivative(G,u)==0
 
 assumptions
 
-sols = dsolve(eqn)%,'Implicit',true,'IgnoreAnalyticConstraints',false)
+sols = dsolve(eqn,x_R,'Implicit',true,'IgnoreAnalyticConstraints',false)
 
 
 
@@ -47,6 +47,3 @@ sols = dsolve(eqn)%,'Implicit',true,'IgnoreAnalyticConstraints',false)
 % f=sqrt((1+diff(y)^2)/(2*g*y));
 % eqn=functionalDerivative(f,y)==0;
 % eqn=simplify(eqn)
-
-(8983381526791143*exp(-(((6*x_R)/5 - 20)/(diff(u(x_R), x_R) + 1)^(1/2) + ((u(x_R) - 1)*diff(u(x_R), x_R))/(diff(u(x_R), x_R) + 1)^(1/2))^2/((2*abs(u(x_R) - 1)^2)/9 + (2*abs((6*x_R)/5 - 20)^2)/9))*exp(-(25*((abs(u(x_R) - 1)^2 + abs((6*x_R)/5 - 20)^2)^(1/2) - 12/5)^2)/32))/(7525900109056348*(abs(u(x_R) - 1)^2 + abs((6*x_R)/5 - 20)^2)^(1/2))
- 
