0d17268160146524b3fc0798882acc0b0da20921
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-11-16 15:35:11 +0900

    AutoPush

--- matlab_ws/1116/MASTER_iter.m
+++ matlab_ws/1116/MASTER_iter.m
@@ -15,7 +15,7 @@ for candidate =[NaN]
     for candidate2=[NaN]
         % try
             savename=string(savedir+"\"+datestr(now,'yymmdd_hhMMss'));
-            graph_title="DEV debug";
+            graph_title="DEV debug or all";
 
             %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
             %                           seq.0  環境                                   %
