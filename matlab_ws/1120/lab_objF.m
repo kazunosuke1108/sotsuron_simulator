1a28b68b6b641d9b9e58e1d1c4d6e132d142d545
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-11-24 10:56:17 +0900

    AutoPush

--- matlab_ws/1120/lab_objF.m
+++ matlab_ws/1120/lab_objF.m
@@ -2,23 +2,47 @@
 
 % simulator for objF.m
 
-clc;clear;
+clc;clear;clf;
 sns=getSensorParams();
+hmn=getHumanParams(sns);
+norm_HR=linspace(0,sns.r2+2,100)
 
-r=linspace(0,sns.r2+2,100)
+
+
+r_11=norm_HR>=sns.r1;
+r_12=norm_HR<sns.r1+2*hmn.sizer;
+r_1=r_11.*r_12;
+score_r1=1/(2*hmn.sizer)*(norm_HR-sns.r1).*r_1;
+
+% 頂上
+r_21=norm_HR>=sns.r1+2*hmn.sizer;
+r_22=norm_HR<sns.r2-2*hmn.sizer;
+r_2=r_21.*r_22;
+score_r2=1.*r_2;
+
+% 下り
+r_31=norm_HR>=sns.r2-2*hmn.sizer;
+r_32=norm_HR<sns.r2;
+r_3=r_31.*r_32;
+score_r3=-1/(2*hmn.sizer)*(norm_HR-sns.r2).*r_3;
+
+% まとめ
+score_r=score_r1+score_r2+score_r3;
+plot(norm_HR,score_r)
+hold on
+
 
 a_small=3;
-smallside_sigmoid=2*(1+exp(-a_small*(r-sns.r1))).^(-1)-1;
+smallside_sigmoid=(1+exp(-a_small*(norm_HR-sns.r1))).^(-1)-1;
 a_big=-3;
-bigside_sigmoid=(1+exp(-a_big*(r-sns.r2))).^(-1);
+bigside_sigmoid=(1+exp(-a_big*(norm_HR-sns.r2))).^(-1);
 
-figure(1),clf;
-plot(r,smallside_sigmoid,'r');
+plot(norm_HR,smallside_sigmoid,'r');
 hold on
-plot(r,bigside_sigmoid,'b');
+plot(norm_HR,bigside_sigmoid,'b');
 hold on
-plot(r,smallside_sigmoid+bigside_sigmoid,'k');
-legend("small-side","big-side","sum");
+plot(norm_HR,smallside_sigmoid+bigside_sigmoid,'k');
+% legend("small-side","big-side","sum");
 
 phi=linspace(0,2*pi,100);
 
