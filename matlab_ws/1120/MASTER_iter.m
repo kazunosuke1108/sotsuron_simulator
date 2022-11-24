acf5bf0bbc6e855eb2989219b1bc7f777bde514e
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-11-24 12:15:43 +0900

    AutoPush

--- matlab_ws/1120/MASTER_iter.m
+++ matlab_ws/1120/MASTER_iter.m
@@ -18,7 +18,7 @@ for candidate =[1]
     for candidate2=[1]
         % try
             savename=string(savedir+"\"+datestr(now,'yymmdd_hhMMss'));
-            graph_title="env.xmax=20 env.avoid_dist=0 env.L=10 env.l=4";
+            graph_title="env.xmax=20 env.L=10 env.l=2";
 
             %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
             %                           seq.0  環境                                   %
@@ -35,7 +35,7 @@ for candidate =[1]
             env.xmax=20;
             % env.avoid_dist=0;
             env.L=10;
-            env.l=4;
+            env.l=2;
             rbt.vx0=0.15;
             rbt.vy0=0;
             % hmn.vx=-0.6;
