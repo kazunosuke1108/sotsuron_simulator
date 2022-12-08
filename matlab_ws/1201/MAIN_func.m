8f315cb929e444e808d62c9a630430103e653da6
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-12-08 12:07:50 +0900

    WIP pc2020e

--- matlab_ws/1201/MAIN_func.m
+++ matlab_ws/1201/MAIN_func.m
@@ -43,7 +43,7 @@ function result=MAIN_func()
     hmn=getHumanParams(sns);
 
     %% overwrite variables
-
+    
     %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
     %                           seq.1  検知                                   %
     %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
@@ -57,10 +57,10 @@ function result=MAIN_func()
     % env.roi.xmin=rbt.x0;
     % env.roi.xmax=env.roi.xmin+env.L;
     % rbt.xF=env.roi.xmax;
-
+    
     % シミュレーション
     [env sns rbt hmn]=sim_detection(rbt,hmn,sns,env);
-
+    
     % 計測所要時間の推定
     %% ロボットの走行所要時間
     t_rbt=abs(env.L/rbt.vxmax);
@@ -68,38 +68,38 @@ function result=MAIN_func()
     t_slack=0.05;
     env.final_tmin=(t_rbt+t_measure)*(1-t_slack);
     env.final_tmax=(t_rbt+t_measure)*(1+t_slack);
-
+    
     %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
     %                           seq.2  計測                                   %
     %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
     % Set up function handles
     problem.func.dynamics=@(t,z,u)(dynamics(z,u,env,rbt,hmn,sns));
     problem.func.pathObj=@(t,z,u)(objF_line(t,z,u,env,rbt,hmn,sns));
-
+    
     % Set up problem bounds
     problem.bounds.initialTime.low = env.init_tmin;
     problem.bounds.initialTime.upp = env.init_tmax;
     problem.bounds.finalTime.low = env.final_tmin;
     problem.bounds.finalTime.upp = env.final_tmax;
-
+    
     problem.bounds.initialState.low = [rbt.x0;rbt.y0;rbt.th0;rbt.vx0;rbt.vy0;rbt.omg0];
     problem.bounds.initialState.upp = [rbt.x0;rbt.y0;rbt.th0;rbt.vx0;rbt.vy0;rbt.omg0];
     problem.bounds.finalState.low = [rbt.xF;rbt.yF;rbt.thFmin;rbt.vx0;rbt.vy0;rbt.omg0];
     problem.bounds.finalState.upp = [rbt.xF;rbt.yF;rbt.thFmax;rbt.vx0;rbt.vy0;rbt.omg0];
-
+    
     problem.bounds.state.low = [rbt.x0;env.ymin+rbt.sizer;rbt.thFmin;rbt.vxmin;rbt.vymin;rbt.omgmin];
     problem.bounds.state.upp = [rbt.xF;env.ymax-rbt.sizer;rbt.thFmax;rbt.vxmax;rbt.vymax;rbt.omgmax];
-
+    
     problem.bounds.control.low = [rbt.axmin;rbt.aymin;rbt.aangmin];
     problem.bounds.control.upp = [rbt.axmax;rbt.aymax;rbt.aangmax];
-
-
+    
+    
     % Initial guess at trajectory
     problem.guess.time = [(problem.bounds.initialTime.low+problem.bounds.initialTime.upp)/2,(problem.bounds.finalTime.low+problem.bounds.finalTime.upp)/2];
     problem.guess.state = [problem.bounds.initialState.low, problem.bounds.finalState.upp];
     problem.guess.control = [0,0;0,0;0,0];
-
-
+    
+    
     % Solver options
     problem.options.nlpOpt = optimset(...
     'Display','iter',...
@@ -108,15 +108,16 @@ function result=MAIN_func()
     'TolX',1e-10,... % x に関する許容誤差 (正のスカラー)
     'TolCon',1e-8,... % 制約違反に関する許容誤差 (正のスカラー)
     'MaxFunEvals',1e6);
-
-
+    
+    
     % problem.options.method = 'trapezoid'; 
     problem.options.method = 'hermiteSimpson';  
-
-
+    problem.options.hermiteSimpson.nSegment = fix(env.final_tmin)
+    
+    
     % Solve!
     soln = trajOpt(problem);
-
+    
 
     
     % Display Solution
@@ -143,10 +144,10 @@ function result=MAIN_func()
     drawPath(t,z,u,env,rbt,hmn,sns,soln,savename,graph_title);
     savename_3_path = savename+"_3_path.png";
     saveas(figure(3),savename_3_path);
-
+    
     result.z=z;
     result.t=t;
-
+    
     % Summarize conditions & results
     save(savename+".mat");
     % writeCSV(problem,env,rbt,hmn,sns,soln,savename);
