090fff6a14cbae7dc1da9b8a5e09576d4289dcec
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-12-24 19:14:41 +0900

    first commit

---
+++ matlab_ws/1225/MAIN_func_iter_pdf.m
@@ -0,0 +1,212 @@
+function result=MAIN_func_iter_pdf()
+    % MAIN.m
+    %% initialization
+    clc; clear;
+    exp_mode=0
+    addpath 'C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\tutorial\cartPole';
+    addpath '/home/ytpc2022h/kazu_ws/sotsuron_simulator/matlab_ws/tutorial/cartPole';
+    addpath '/home/ytpc2022h/catkin_ws/src/sotsuron_simulator/matlab_ws/tutorial/cartPole';
+    addpath 'C:\Users\hyper\OneDrive\デスクトップ\VSCode\sotsuron_simulator\matlab_ws\tutorial\cartPole';
+    addpath '/home/hayashide/kazu_ws/sotsuron_simulator/matlab_ws/tutorial/cartPole';
+    addpath '/home/hayashide/catkin_ws/src/sotsuron_simulator/matlab_ws/tutorial/cartPole';
+    addpath '/home/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/scripts/monitor';
+    addpath '/home/hayashide/catkin_ws/src/sotsuron_experiment/sotsuron_experiment/scripts/monitor';
+
+    % addpath 'C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\tutorial\cartPole';
+    addpath 'C:\Users\林出和之\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\tutorial\cartPole'
+    for candidate=[10]%xmax
+        for candidate2=[-4.5]%ymin
+            for candidate3=-0.6:-0.01:-1.21%vx
+                for candidate4=[0.05 0.10 0.15 0.20 0.25 0.3 0.35 0.4]%slack
+                    % for candidate5=[-1 -1.5 -2 -2.5 -3]%rbt.y0
+                        try
+                            date="1222";
+                            abst="parameter_study_pdf";
+                            detail="x10_y4.5_hmnvx"+string(abs(candidate3))+"_slack"+string(candidate4);
+                            mkdir('results');
+                            % savedir="results\"+date+"_"+abst;
+                            savedir="results/"+date+"_"+abst;
+                            mkdir(savedir);
+                            % savedir=string(savedir+"\"+datestr(now,'yymmdd_hhMMss')+"_"+detail);
+                            savedir=string(savedir+"/"+datestr(now,'yymmdd_hhMMss')+"_"+detail);
+                            mkdir(savedir);
+
+                            % for ...
+
+                            graph_title="test";
+                            % savename=string(savedir+"\"+datestr(now,'yymmdd_hhMMss')+"_"+graph_title);
+                            savename=string(savedir+"/"+datestr(now,'yymmdd_hhMMss')+"_"+graph_title);
+
+                            %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+                            %                           seq.0  環境                                   %
+                            %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+                            %% load default variables
+                            env=getEnvironmentParams();
+                            sns=getSensorParams();
+                            rbt=getRobotParams(env);
+                            hmn=getHumanParams(env,sns);
+                            %% overwrite variables
+
+                            env.xmax=candidate;
+                            env.roi.xmax=env.roi.xmin+env.L;
+                            rbt.xF=env.xmax;
+
+                            env.ymin=candidate2;
+                            env.kabe.ymin=env.ymin;
+                            env.roi.ymin=env.ymin;
+
+                            hmn.x0=env.xmax;
+
+                            hmn.vx=candidate3;
+
+                            t_slack=candidate4;
+
+                            env.hz=abs(hmn.vx)*40/3;
+                            % rbt.vxmin=-rbt.vxmax;
+                            
+                            %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+                            %                           seq.1  検知                                   %
+                            %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+                            % 実機
+                            %% jsonから人の位置・速度を取得
+                            if exp_mode
+                                [env.dist_zed_hmn,hmn.vx]=getHumanVelocity();
+                                tic;
+                                env.publish_time=(env.dist_hsr_zed+env.dist_zed_hmn-env.L)/abs(hmn.vx);
+                            end
+                            %% hmn_path
+                            % json=jsondecode(fileread('/home/hayashide/catkin_ws/src/sotsuron_experiment/scripts/monitor/velocity.json'));
+                            % % json=jsondecode(fileread('/home/hayashide/kazu_ws/sotsuron_experiment/sotsuron_experiment/scripts/monitor/velocity.json'))
+                            % hmn.x0=json.z_linear_z;
+                            % hmn.vx=json.z_linear_a;
+                            % env.roi.xmin=rbt.x0;
+                            % env.roi.xmax=env.roi.xmin+env.L;
+                            % rbt.xF=env.roi.xmax;
+                            
+                            % シミュレーション
+                            % [env sns rbt hmn]=sim_detection(rbt,hmn,sns,env);
+                            
+                            % 計測所要時間の推定
+                            %% ロボットの走行所要時間
+                            t_rbt=abs(env.L/rbt.vxmax);
+                            t_measure=abs(env.l/hmn.vx); % env.l=ロボットが立ち止まって人を計測したい歩行距離
+                            % t_slack=0.05;
+                            env.estim_final_t=t_rbt+t_measure;
+                            env.final_tmin=env.estim_final_t*(1-t_slack);
+                            env.final_tmax=env.estim_final_t*(1+t_slack);
+                            
+                            % 標準制御周波数の決定
+                            problem.options.hermiteSimpson.nSegment=fix((env.estim_final_t*env.hz-1)/30);
+
+
+                            %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+                            %                           seq.2  計測                                   %
+                            %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+                            % Set up function handles
+                            problem.func.dynamics=@(t,z,u)(dynamics(z,u,env,rbt,hmn,sns));
+                            % problem.func.pathObj=@(t,z,u)(objF_line(t,z,u,env,rbt,hmn,sns));
+                            problem.func.pathObj=@(t,z,u)(objF_line_pdf(t,z,u,env,rbt,hmn,sns));
+                            problem.func.pathCst=@(t,z,u)(constraint(t,z,u,env,rbt,hmn,sns));
+                            
+                            % Set up problem bounds
+                            problem.bounds.initialTime.low = env.init_tmin;
+                            problem.bounds.initialTime.upp = env.init_tmax;
+                            problem.bounds.finalTime.low = env.final_tmin;
+                            problem.bounds.finalTime.upp = env.final_tmax;
+                            
+                            problem.bounds.initialState.low = [rbt.x0;rbt.y0;rbt.th0;rbt.vx0;rbt.vy0;rbt.omg0];
+                            problem.bounds.initialState.upp = [rbt.x0;rbt.y0;rbt.th0;rbt.vx0;rbt.vy0;rbt.omg0];
+                            problem.bounds.finalState.low = [rbt.xF;rbt.yF;rbt.thFmin;rbt.vx0;rbt.vy0;rbt.omg0];
+                            problem.bounds.finalState.upp = [rbt.xF;rbt.yF;rbt.thFmax;rbt.vx0;rbt.vy0;rbt.omg0];
+                            
+                            problem.bounds.state.low = [rbt.x0;env.ymin+rbt.sizer;rbt.thFmin;rbt.vxmin;rbt.vymin;rbt.omgmin];
+                            problem.bounds.state.upp = [rbt.xF;env.ymax-rbt.sizer;rbt.thFmax;rbt.vxmax;rbt.vymax;rbt.omgmax];
+                            
+                            problem.bounds.control.low = [rbt.axmin;rbt.aymin;rbt.aangmin];
+                            problem.bounds.control.upp = [rbt.axmax;rbt.aymax;rbt.aangmax];
+                            
+                            
+                            % Initial guess at trajectory
+                            problem.guess.time = [(problem.bounds.initialTime.low+problem.bounds.initialTime.upp)/2,(problem.bounds.finalTime.low+problem.bounds.finalTime.upp)/2];
+                            problem.guess.state = [problem.bounds.initialState.low, problem.bounds.finalState.upp];
+                            problem.guess.control = [0,0;0,0;0,0];
+                            
+                            
+                            % Solver options
+                            problem.options.nlpOpt = optimset(...
+                            'Display','iter',...
+                            'MaxIter',1e3,... % 可能な反復の最大数 (正の整数)
+                            'TolFun',1e-12,... % 1 次の最適性に関する終了許容誤差 (正のスカラー)
+                            'TolX',1e-10,... % x に関する許容誤差 (正のスカラー)
+                            'TolCon',1e-8,... % 制約違反に関する許容誤差 (正のスカラー)
+                            'MaxFunEvals',1e6);
+                            
+                            % problem.options.method = 'trapezoid'; 
+                            problem.options.method = 'hermiteSimpson';  
+                            % problem.options.hermiteSimpson.nSegment=10;
+                            
+                            
+                            % Solve!
+                            soln = trajOpt(problem);
+                            
+                            
+
+                            
+                            % Display Solution
+                            n = length(soln.grid.time);
+                            t = linspace(soln.grid.time(1), soln.grid.time(end), 15*(n-1)+1);
+                            z = soln.interp.state(t);
+                            u = soln.interp.control(t);
+                            
+                            % Plots
+                            %% add score to fig name
+                            save(savename+".mat");
+                            graph_title=graph_title+" J="+soln.info.bestfeasible.fval;
+                            %% History
+                            if exp_mode
+                                pass
+                            else
+                                figure(1); clf;
+                                pltHistory(t,z,u,env,rbt,hmn,sns,soln,graph_title);
+                                savename_png = savename+"_3_hist.png";
+                                saveas(figure(1),savename_png);
+                                
+                                %% Animation
+                                figure(2); clf;
+                                savename_3_anim=savename+"_3_anim";
+                                drawAnimation(t,z,u,env,rbt,hmn,sns,soln,savename_3_anim,graph_title);
+                                
+                                figure(3); clf;
+                                drawPath(t,z,u,env,rbt,hmn,sns,soln,savename,graph_title);
+                                savename_3_path = savename+"_3_path.png";
+                                saveas(figure(3),savename_3_path);
+                            end
+                            result.z=z;
+                            result.t=t;
+                            
+                            % Summarize conditions & results
+                            if exp_mode
+                                start_waiting=toc;
+                                while 1
+                                    if toc>=env.publish_time
+                                        disp("Publish. current time:"+string(toc)+" publish time:"+string(env.publish_time)+" calc time:"+string(soln.info.nlpTime)+" start waiting since:"+string(start_waiting))
+                                        break
+                                    else
+                                        if mod(toc,0.01)==0
+                                            disp("Waiting for publish. current time:"+string(toc)+" publish time:"+string(env.publish_time))
+                                        end
+                                    end
+                                end
+                            end
+                        catch
+                            save(savename+".mat");
+                            continue
+                        end
+                    % end
+                end
+            end
+        end
+    end
+    git_auto_push()
+    % writeCSV(problem,env,rbt,hmn,sns,soln,savename);
---
+++ matlab_ws/1225/kalman_1221.m
@@ -0,0 +1,189 @@
+clc;clear;
+fig=figure(1);
+data=readmatrix("C:\Users\hayashide\Desktop\kazu_ws\sotsuron_experiment\sotsuron_experiment\scripts\kalman\track_results_1216_090.csv");
+for i = 1:length(data(:,1))
+    if data(i,4)<10
+        break
+    end
+end
+data=data([i:end],:);
+
+estm_history=[]
+
+for i = length(data(:,1))
+    partial_data=data([1:i],:);
+
+    t=partial_data(:,1)-partial_data(1,1);
+    z=partial_data(:,4);
+
+    %% LPF
+    cutoff=0.001;
+    fps=15;
+    z_lpf=LPF(z,cutoff,fps);
+    cutframe=1;
+    z_lpf=z_lpf(cutframe:end-cutframe);
+    t_lpf=t(cutframe:end-cutframe);
+
+    % for Q=[0.001 0.002 0.003 0.004]
+    for Q=[0.001 0.002 0.003 0.004]
+        for R= 1e-20
+                %% load data
+                subplot(3,1,1)
+                plot(t,z,'k','LineWidth',1)
+                hold on
+                
+                plot(t_lpf,z_lpf,'r','LineWidth',3)
+                hold on
+                po=z_lpf;
+
+                %% Kalman Filter
+                pv0=0;
+                % Q=0.0041133;
+                % R=1e+9;
+                N=0;
+                % estm_list=kalman_filter(po,pv0,Q,R,N);
+                estm_list=kalman_filter_ohmori(po,pv0,Q,R,N);
+                % estm_list=kalman_filter_zihen(po,pv0,Q,R,N,t);
+                plot(t_lpf,estm_list(1,:))
+                hold on
+                xlabel("time [s]")
+                ylabel("position [m]")
+
+                %% Velocity
+                % remove trend
+                % estm_list(2,:)=detrend(estm_list(2,:)); % 真値は等速の仮定より，経時的な値の変動を補正する
+                subplot(3,1,2)
+                plot(t_lpf,estm_list(2,:))
+                hold on
+                xlabel("time [s]")
+                ylabel("velocity [m/s]")
+
+        end
+    end
+    % plot(t,-1.2*ones(1,length(t)))
+    plot(t,-0.9*ones(1,length(t)))
+    localmin=islocalmin(estm_list(2,:));
+    localmin_idx=find(localmin>0.5);
+    estm_vel=median(estm_list(2,[localmin_idx(1):localmin_idx(end)]))
+    estm_history=[estm_history estm_vel];
+
+end
+
+subplot(3,1,3)
+plot(1:length(estm_history),estm_history)
+estm_list(1,end)
+
+% 最後の速度をreturnするのが妥当．なるべく長時間推定したい感はある
+% 1.5m切ったら終了
+
+function z_lpf=LPF(z,cutoff,fps)
+    %%% 平均を0に持っていく
+    ave=mean(z);
+    z=z-ave;
+    %%% LPF
+    [z_lpf,d]=lowpass(z,cutoff,fps);
+    %%% 平均を元に戻す
+    z_lpf=z_lpf+ave;
+end
+
+% function estm_list=kalman_filter_zihen(po,pv0,Q,R,N,t)
+%     %% variables
+%     po=po;
+%     estm_list=zeros(2,length(po));
+    
+%     pHat_k_km1=[po(1);pv0];
+%     i=1;
+%     for po_scaler=po.'
+%         try
+%             fps=1/(t(i+1)-t(i));
+%             % disp(fps)
+%         catch
+%             fps=15;
+%         end
+%         % system data
+%         % fps=15;
+%         A=[1 1/fps;0 1];
+%         B=[0;1];
+%         C=[1 0.1];
+%         D=0;
+    
+%         Plant = ss(A,B,C,D,-1);
+%         Plant.InputName = 'un';
+%         Plant.OutputName = 'yt';
+%         Sum = sumblk('un = u + w');
+%         sys = connect(Plant,Sum,{'u','w'},'yt');
+%         %% Filtering
+%         sensors=[1];
+%         % [kalmf,L,P] = kalman(sys,Q,R,N);
+%         [kalmf,L,P] = kalman(sys,Q,R,N,sensors);
+%         pHat_k_k=pHat_k_km1+L*(po_scaler-C*pHat_k_km1);
+%         pHat_kp1_k=A*pHat_k_k;
+%         estm_list(:,i)=pHat_kp1_k;
+%         pHat_k_km1=pHat_k_k;
+%         pHat_k_k=pHat_kp1_k;
+%         i=i+1;
+%     end
+% end
+
+function estm_list=kalman_filter(po,pv0,Q,R,N)
+    %% variables
+    po=po;
+    
+    %% system data
+    fps=15;
+    A=[1 1/fps;0 1];
+    B=[0;1];
+    C=[1 0];
+    D=0;
+
+    Plant = ss(A,B,C,D,-1);
+    Plant.InputName = 'un';
+    Plant.OutputName = 'yt';
+    Sum = sumblk('un = u + w');
+    sys = connect(Plant,Sum,{'u','w'},'yt');
+
+    %% Filtering
+    sensors=[1];
+    [kalmf,L,P] = kalman(sys,Q,R,N,sensors);
+    estm_list=zeros(2,length(po));
+    disp(P)
+    pHat_k_km1=[po(1);pv0];
+    i=1;
+    for po_scaler=po.'
+        pHat_k_k=pHat_k_km1+L*(po_scaler-C*pHat_k_km1);
+        pHat_kp1_k=A*pHat_k_k;
+        estm_list(:,i)=pHat_kp1_k;
+        pHat_k_km1=pHat_k_k;
+        pHat_k_k=pHat_kp1_k;
+        i=i+1;
+    end
+end
+
+function estm_list=kalman_filter_ohmori(po,pv0,Q,R,N)
+    %% system data
+    fps=15;
+    A=[1 1/fps;0 1];
+    B=[0;1];
+    C=[1 0];
+    D=0;
+
+    Plant = ss(A,B,C,D,-1);
+    Plant.InputName = 'un';
+    Plant.OutputName = 'yt';
+    Sum = sumblk('un = u + w');
+    sys = connect(Plant,Sum,{'u','w'},'yt');
+
+    %% Filtering
+    sensors=[1];
+    [kalmf,L,P] = kalman(sys,Q,R,N,sensors);
+    estm_list=zeros(2,length(po));
+
+    pHat_k=[po(1);pv0];
+    i=1;
+    for po_scaler = po.'
+        pHat_kp1=(A-L*C)*pHat_k+po_scaler*L;
+        estm_list(:,i)=pHat_kp1;
+        pHat_k=pHat_kp1;
+        i=i+1;
+    end
+end
