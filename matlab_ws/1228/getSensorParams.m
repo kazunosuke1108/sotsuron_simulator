bc6ba67cb1c4704c7a28f26ed176ac470afd8121
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-12-28 14:47:41 +0900

    first commit 1228

---
+++ matlab_ws/1228/getSensorParams.m
@@ -0,0 +1,17 @@
+function sns=getSensorParams()
+
+    sns.h=0.8;
+    
+    sns.phi=86;
+    % sns.phi=58;
+    sns.pitch=57;
+    sns.pitch=86;
+    % sns.pitch=45;
+    sns.r0=6.0;
+    sns.r2=6.0;
+    
+    sns.phi=deg2rad(sns.phi)/2;
+    sns.pitch=deg2rad(sns.pitch)/2;
+    sns.r1=sns.h/tan(sns.pitch);
+
+end
