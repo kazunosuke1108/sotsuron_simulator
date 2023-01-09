14b468d7fa070f05879b1310d1ecd4444e4771cf
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-01-09 20:21:29 +0900

    WIP

--- matlab_ws/230106/visualizer.m
+++ matlab_ws/230106/visualizer.m
@@ -1,7 +1,7 @@
 addpath 'C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\tutorial\cartPole';
 % addpath '/home/hayashide/kazu_ws/sotsuron_simulator/matlab_ws/tutorial/cartPole'
 
-matpath="C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\230106\results\230107_1340_parameter_study_d455\230107_154228_d_hmny0_2.5_vx1\230107_154228_.mat";
+matpath="C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\230106\results\230109_1435_parameter_study_d455\230109_143343_d_hmny0_0.5_vx0.6\230109_143343_.mat";
 % matpath="/home/hayashide/kazu_ws/sotsuron_simulator/matlab_ws/1210/results/1210_parastd4Hz/221210_140704_xmax10_ymin-2/221210_140704_test.mat"
 load(matpath)
 
