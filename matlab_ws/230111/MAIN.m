18b78ec74617e7dbc14936d7a20b7cb62178bf22
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-01-12 14:48:20 +0900

    AutoPush

--- matlab_ws/230111/MAIN.m
+++ matlab_ws/230111/MAIN.m
@@ -1,6 +1,6 @@
 clc;clear;
-res=MAIN_func();
-% res=MAIN_func_iter0111();
+% res=MAIN_func();
+res=MAIN_func_iter0111();
 % res=MAIN_func_iter0111_remote();
 % res=MAIN_func_iter0106_NIGHTFIGHTER();
 % result_gather();
