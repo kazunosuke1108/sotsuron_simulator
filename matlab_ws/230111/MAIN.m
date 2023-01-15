deb89d4aefd11938c2c007c25c834737730784e2
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-01-15 14:33:59 +0900

    npc2021i

--- matlab_ws/230111/MAIN.m
+++ matlab_ws/230111/MAIN.m
@@ -1,7 +1,7 @@
 clc;clear;
-res=MAIN_func();
+% res=MAIN_func();
 % res=MAIN_func_iter0111();
-% res=MAIN_func_iter0111_remote();
+res=MAIN_func_iter0111_remote();
 % res=MAIN_func_iter0106_NIGHTFIGHTER();
 % result_gather();
 % git_auto_push()3.608145
