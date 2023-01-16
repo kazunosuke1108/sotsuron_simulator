4418060f9a6221ed53cab2f63ac2c4b56a7f0c9c
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-01-16 13:44:57 +0900

    2021c

--- matlab_ws/230115/result_gather.m
+++ matlab_ws/230115/result_gather.m
@@ -1,6 +1,6 @@
 clc;clear;
 
-motherdir="C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\230115\results\2021i_230116_0000_parameter_study_d455_rapid_vx010";
+motherdir="C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\230115\results\2022h_230116_0000_parameter_study_d455";
 dirlist=dir(motherdir);
 figure(1); clf;
 
