6805064cdab42d2b0d9bcb23d06fdbb0cbcb9b3f
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-01-17 16:55:54 +0900

    final results

--- matlab_ws/230117/MAIN.m
+++ matlab_ws/230117/MAIN.m
@@ -1,12 +1,7 @@
 clc;clear;
 % res=MAIN_func();
-<<<<<<< HEAD
 % res=MAIN_func_iter0117();
 res=MAIN_func_iter0117_remote();
-=======
-res=MAIN_func_iter0117();
-% res=MAIN_func_iter0111_remote();
->>>>>>> 8390789beb99ddb60bd05c4b99bd0e41253fb80a
 % res=MAIN_func_iter_rapid();
 % result_gather();
 % git_auto_push();
