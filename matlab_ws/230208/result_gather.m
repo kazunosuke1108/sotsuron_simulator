d661bca422022fa9e8845e0db17db3536c1c9d12
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-02-12 22:39:35 +0900

    AutoPush

--- matlab_ws/230208/result_gather.m
+++ matlab_ws/230208/result_gather.m
@@ -1,6 +1,6 @@
 clc;clear;
 
-motherdir="C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\230208\results\2021d_230211_1900_parameter_study_d455_ymax_3";
+motherdir="C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\230208\results\2022e_230211_1900_parameter_study_d455_ymax_5";
 % motherdir="C:\Users\林出和之\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\230121\results\2022h_230122_1800_parameter_study_d455_no_offset";
 dirlist=dir(motherdir);
 figure(1); clf;
