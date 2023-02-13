be1c8f8341b05bbf05499b5f255f2fb1a22a6791
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-02-13 19:09:22 +0900

    wip

--- matlab_ws/230208/MAIN_func_iter0211_ymax4.m
+++ matlab_ws/230208/MAIN_func_iter0211_ymax4.m
@@ -24,7 +24,7 @@ function result=MAIN_func_iter0207()
                         abst="0000_parameter_study_LRF";
                         detail="L_hmny0_"+string(abs(candidate3))+"_vx"+string(abs(candidate));
                     else
-                        abst="1800_parameter_study_d455_ymax_4_back";
+                        abst="1800_parameter_study_d455_ymax_3.5";
                         detail="d_hmny0_"+string(abs(candidate3))+"_vx"+string(abs(candidate));
                     end
                     mkdir('results');
@@ -100,7 +100,7 @@ function result=MAIN_func_iter0207()
                     env.L=15;
                     env.xmax=env.L;
                     env.roi.xmax=env.L;
-                    env.ymax=4;
+                    env.ymax=3.5;
                     env.kabe.ymax=env.ymax;
                     env.roi.ymax=env.ymax;
                 
