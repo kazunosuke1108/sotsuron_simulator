7f0b93c36289473641352a4b4466c96d4bbe1a7f
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-02-19 16:44:42 +0900

    WIP

--- matlab_ws/230208/MAIN_func.m
+++ matlab_ws/230208/MAIN_func.m
@@ -19,8 +19,8 @@ function result=MAIN_func()
                 exp_mode=1;
                 LRF_mode=0; % 0:d455 1:LRF
                 date="230219";
-                abst="7F_zed";
-                detail="20230219_y220_v090_01";
+                abst="7F_zed_thre1.8";
+                detail="20230219_y025_v090_01";
                 mkdir('results');
                 % savedir="results\"+date+"_"+abst;
                 savedir="results/"+date+"_"+abst;
@@ -307,7 +307,7 @@ function result=MAIN_func()
                             disp("Publish. current time:"+string(toc)+" publish time:"+string(env.publish_time)+" calc time:"+string(soln.info.nlpTime)+" start waiting since:"+string(start_waiting))
                             break
                         else
-                            if mod(toc,0.01)==0
+                            if mod(toc,0.1)==0
                                 disp("Waiting for publish. current time:"+string(toc)+" publish time:"+string(env.publish_time))
                             end
                         end
