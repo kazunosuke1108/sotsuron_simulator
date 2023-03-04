adadb86ab9fd351aacda08302fb75c3b7cc90065
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-03-04 15:40:34 +0900

    AutoPush

--- matlab_ws/230304/MAIN_func.m
+++ matlab_ws/230304/MAIN_func.m
@@ -112,7 +112,7 @@ function result=MAIN_func()
                 hmn.personal_r=0.6;
 
                 hmn.vx=-0.9;
-                hmn.y0=1.5;
+                hmn.y0=1;
                 
                 if exp_mode
                     [env.dist_zed_hmn,hmn.y0,hmn.vx]=getHumanVelocity(env);
