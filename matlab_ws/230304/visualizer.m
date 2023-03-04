88dfd7fb754f82b43f5ce46dd3d27ba36bc0d737
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-03-04 12:38:44 +0900

    WIP

--- matlab_ws/230304/visualizer.m
+++ matlab_ws/230304/visualizer.m
@@ -2,14 +2,14 @@ addpath 'C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\tutoria
 addpath 'C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\230117\final_results';
 % addpath '/home/hayashide/kazu_ws/sotsuron_simulator/matlab_ws/tutorial/cartPole'
 
-matpath="C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\230208\results\230220_shingo\230220_202624_20230220_y040_v000_30\230220_202624_.mat";
+matpath="C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\230304\results\230304_2022e_1100_parameter_study_d455_tate_personalr_060\230304_104931_d_hmny0_0.5_vx0.9\230304_104931_.mat";
 % matpath="/home/hayashide/kazu_ws/sotsuron_simulator/matlab_ws/1210/results/1210_parastd4Hz/221210_140704_xmax10_ymin-2/221210_140704_test.mat"
 load(matpath)
 
 
-date="230225";
+date="230304";
 abst="visualizer";
-detail="230220_202624_";
+detail="230304_104931_";
 mkdir('results');
 savedir="results\"+date+"_"+abst;
 % savedir="results/"+date+"_"+abst;
