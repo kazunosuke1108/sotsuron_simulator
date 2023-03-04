28566efcf6750c1a9420204d5e2c9e7409a9bd7b
HAYASHIDE KAZUYUKI <hyperturbo.kazunosuke@gmail.com>
2023-03-04 10:53:24 +0900

    AutoPush

--- matlab_ws/230304/MAIN.m
+++ matlab_ws/230304/MAIN.m
@@ -1,7 +1,7 @@
 clc;clear;
 % res=MAIN_func();
 % res=MAIN_func_iter0211();
-res=MAIN_func_iter0211_ymax28();
+res=MAIN_func_iter0304();
 % res=MAIN_func_iter_rapid();
 % result_gather();
 git_auto_push();
