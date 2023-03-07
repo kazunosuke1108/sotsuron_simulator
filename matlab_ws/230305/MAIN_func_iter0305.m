d93713b391ea8dad1531b517c1385b440a6d6b54
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-03-07 12:02:06 +0900

    WIP

--- matlab_ws/230305/MAIN_func_iter0305.m
+++ matlab_ws/230305/MAIN_func_iter0305.m
@@ -19,12 +19,12 @@ function result=MAIN_func_iter0305()
                     %% experiment or simulation
                     exp_mode=0;
                     LRF_mode=candidate2; % 0:d455 1:LRF
-                    date="230307_2022e";
+                    date="230307_2022h";
                     if LRF_mode
                         abst="0000_parameter_study_LRF";
                         detail="L_hmny0_"+string(abs(candidate3))+"_vx"+string(abs(candidate));
                     else
-                        abst="1100_parameter_study_d455_ZED_tate_40_sqp";
+                        abst="1200_parameter_study_d455_ZED_tate_40_sqp";
                         detail="d_hmny0_"+string(abs(candidate3))+"_vx"+string(abs(candidate));
                     end
                     mkdir('results');
