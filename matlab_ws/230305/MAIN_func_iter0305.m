3ac50833a2e325b014ec8ca75fb810ebaa98d830
HAYASHIDE KAZUYUKI <hyperturbo.kazunosuke@gmail.com>
2023-03-07 10:31:42 +0900

    AutoPush

--- matlab_ws/230305/MAIN_func_iter0305.m
+++ matlab_ws/230305/MAIN_func_iter0305.m
@@ -13,7 +13,7 @@ function result=MAIN_func_iter0305()
     addpath 'C:\Users\林出和之\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\tutorial\cartPole'
     % addpath 'C:\Users\hayashide\Desktop\kazu_ws\sotsuron_simulator\matlab_ws\tutorial\cartPole';
     for candidate2=[0]
-        for candidate3=0.5:0.25:4.51
+        for candidate3=0.25:0.25:1.76
             for candidate=-0.6:-0.05:-1.21
                 try
                     %% experiment or simulation
